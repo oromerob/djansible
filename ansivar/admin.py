ae1699434ab8d8bc5ee0c1b6c0668e744dd65fbe
ZUNbado <git@acs.li>
2014-10-28 20:55:21 +0100

    testing

--- ansivar/admin.py
+++ ansivar/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from ansivar.models import Host, VarGroup, VarKey, VarName, VarValue
 
+class VarKeyAdmin(admin.ModelAdmin):
+	list_display = [ 'id', 'host', 'var_group' ]
+
+
+class VarValueAdmin(admin.ModelAdmin):
+	fields = [ 'var_key', 'var_name', 'value' ]
+
 admin.site.register(Host)
 admin.site.register(VarGroup)
-admin.site.register(VarKey)
+admin.site.register(VarKey, VarKeyAdmin)
 admin.site.register(VarName)
 admin.site.register(VarValue)
