9fe149070d1cc1eb025b855b72972828aabf53e8
Oriol Romero <info@elprimoproductions.com>
2014-10-30 19:26:11 +0100

    hosts admin modificat perquè si no hi ha host, es mostri el formulari

--- apps/hosts/admin.py
+++ apps/hosts/admin.py
@@ -10,11 +10,13 @@ class VarInline(admin.StackedInline):
 
     def formfield_for_dbfield(self, field, **kwargs):
         if field.name == 'var_key':
-            parent_host = self.get_object(kwargs['request'], Host)
-            var_keys = []
-            groups = parent_host.var_group.all().distinct()
-            properties = VarKey.objects.filter(var_group__in=groups)
-            return forms.ModelChoiceField(queryset=properties)
+            try:
+                parent_host = self.get_object(kwargs['request'], Host)
+                groups = parent_host.var_group.all().distinct()
+                properties = VarKey.objects.filter(var_group__in=groups)
+                return forms.ModelChoiceField(queryset=properties)
+            except:
+                properties = VarKey.objects.all()
         return super(VarInline, self).formfield_for_dbfield(field, **kwargs)
 
     def get_object(self, request, model):
