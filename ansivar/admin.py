e2399264bebd379141d1634f41a7493d0a1430d4
ZUNbado <git@acs.li>
2014-10-30 19:44:54 +0100

    sqlite test

--- ansivar/admin.py
+++ ansivar/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from ansivar.models import Host, VarGroup, VarKey, VarName, VarValue
 
+
+class VarValueInline(admin.StackedInline):
+	model = VarValue
+
 class VarKeyAdmin(admin.ModelAdmin):
 	list_display = [ 'id', 'host', 'var_group' ]
+	inlines = [ VarValueInline ]
 
 
 class VarValueAdmin(admin.ModelAdmin):
