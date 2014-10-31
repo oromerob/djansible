fd16f9aee81669c73c0d29d72dcbcee89f787ac2
Oriol Romero <info@elprimoproductions.com>
2014-10-31 21:09:38 +0100

    first commit

--- apps/conf/views.py
+++ apps/conf/views.py
@@ -1,22 +1,27 @@
 from django.http import HttpResponse
 import json
 
-from apps.hosts.models import *
-from apps.conf.models import *
+from apps.hosts import models as host_models
+#from apps.conf import models as conf_models
+
 
 def export_json(request):
-	hosts = Host.objects.all()
-	export = {}
-	for host in hosts:
-		if not host in export: export[host.name]={}
-		groups = HostVarGroups.objects.filter(host=host)
-		for group in groups:
-			if not group.var_group.name in export[host.name]: export[host.name][group.var_group.name]=[]
-			values = Var.objects.filter(host_var_group=group)
-			finalvalue={}
-			for value in values:
-				finalvalue[value.var_def.name]=value.value
-			export[host.name][group.var_group.name].append(finalvalue)
-				
-	return HttpResponse(json.dumps(export, indent=3), content_type="application/json")
+    hosts = host_models.Host.objects.all()
+    export = {}
+    for host in hosts:
+        if not host in export:
+            export[host.name] = {}
+        groups = host_models.HostVarGroups.objects.filter(host=host)
+        for group in groups:
+            if not group.var_group.name in export[host.name]:
+                export[host.name][group.var_group.name] = []
+            values = host_models.Var.objects.filter(host_var_group=group)
+            finalvalue = {}
+            for value in values:
+                finalvalue[value.var_def.name] = value.value
+            export[host.name][group.var_group.name].append(finalvalue)
 
+    return HttpResponse(
+        json.dumps(export, indent=3),
+        content_type="application/json"
+    )
